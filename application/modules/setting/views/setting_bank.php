e37be23cf2e83686b09b5a3e51d2c347a7356ac5
Dev Natthanon <73880180+natto1445@users.noreply.github.com>
2024-02-05 18:18:21 +0700

    manage bank

--- application/modules/setting/views/setting_bank.php
+++ application/modules/setting/views/setting_bank.php
@@ -3,118 +3,131 @@
 
 <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Setting</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">ตั้งค่าระบบ</a></li>
-          <li class="breadcrumb-item active">ข้อมูลธนาคาร</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Setting</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">ตั้งค่าระบบ</a></li>
+        <li class="breadcrumb-item active">ข้อมูลธนาคาร</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
 
-    <div class="div" style="margin-bottom: 20px;">
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addType">เพิ่มข้อมูล</button>
-    </div>
+  <div class="div" style="margin-bottom: 20px;">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addBank">เพิ่มข้อมูล</button>
+  </div>
 
 
-    <div class="modal fade" id="addType" tabindex="-1">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">เพิ่มข้อมูล</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <form id="add_product" class="row g-3">
-                <div class="col-12">
-                  <label for="name_type" class="form-label">ชื่อประเภทสินค้า</label>
-                  <input type="text" class="form-control" id="name_type" name="name_type">
-                </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
-            <a class="btn btn-primary" onclick="save('add_product')">บันทึก</a>
-          </div>
+  <div class="modal fade" id="addBank" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">เพิ่มข้อมูล</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-      </div>
-    </div>
-
-    <div class="modal fade" id="editType" tabindex="-1">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">แก้ไขข้อมูล</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <form id="edit_product" class="row g-3">
-                <div class="col-12">
-                  <label for="Ecode_type" class="form-label">รหัสประเภทสินค้า</label>
-                  <input type="text" class="form-control" id="Ecode_type" name="Ecode_type" readonly>
-                  <input type="hidden" id="Eid_type" name="Eid_type">
-                </div>
-                <div class="col-12">
-                  <label for="Ename_type" class="form-label">ชื่อประเภทสินค้า</label>
-                  <input type="text" class="form-control" id="Ename_type" name="Ename_type">
-                </div>
-                <div class="col-12">
-                  <label for="Estatus_type" class="form-label">สถานะ</label>
-                    <select class="form-select" id="Estatus_type" name="Estatus_type" aria-label="Estatus_type">
-                      <?php foreach ($status as $key => $value) {?>
-                        <option value="<?=$key?>"><?=$value?></option>
-                      <?php }?>
-                    </select>
-                </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
-            <a class="btn btn-warning" onclick="edit('edit_product')">แก้ไข</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <section class="section">
-      <div class="row">
-        <div class="col-lg-12">
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">จัดการข้อมูลผู้ใช้งาน</h5>
-
-              <table id='product_data' class="table" style="font-family: 'Kanit', sans-serif;">
-                <thead>
-                  <tr>
-                    <th>ลำดับ</th>
-                    <th>รหัสสินค้า</th>
-                    <th>
-                      <b>ชื่อสินค้า</b>
-                    </th>
-                    <th>ประเภทสินค้า</th>
-                    <th>จำนวน</th>
-                    <th>สต๊อกขั้นต่ำ</th>
-                    <th>ราคาทุน</th>
-                    <th>ราคาขาย</th>
-                    <th>หน่วย</th>
-                    <th>สถานะ</th>
-                    <th>จัดการ</th>
-                  </tr>
-                </thead>
-                <tbody class='data-product'>
-
-                </tbody>
-              </table>
-
+        <div class="modal-body">
+          <form id="add_bank" class="row g-3">
+            <div class="col-6">
+              <label for="bank_code" class="form-label">เลขที่บัญชี</label>
+              <input type="text" class="form-control" id="bank_code" name="bank_code">
             </div>
-          </div>
 
+            <div class="col-6">
+              <label for="bank_name" class="form-label">ธนาคาร</label>
+              <input type="text" class="form-control" id="bank_name" name="bank_name">
+            </div>
+
+            <div class="col-6">
+              <label for="bank_branch" class="form-label">สาขาเปิดบัญชี</label>
+              <input type="text" class="form-control" id="bank_branch" name="bank_branch">
+            </div>
+
+            <div class="col-6">
+              <label for="bank_owner" class="form-label">ชื่อบัญชี</label>
+              <input type="text" class="form-control" id="bank_owner" name="bank_owner">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
+          <a class="btn btn-primary" onclick="save('add_bank')">บันทึก</a>
         </div>
       </div>
-    </section>
+    </div>
+  </div>
+
+  <div class="modal fade" id="editBank" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">แก้ไขข้อมูล</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form id="edit_bank" class="row g-3">
+            <div class="col-12">
+              <label for="Ecode_type" class="form-label">รหัสประเภทสินค้า</label>
+              <input type="text" class="form-control" id="Ecode_type" name="Ecode_type" readonly>
+              <input type="hidden" id="Eid_type" name="Eid_type">
+            </div>
+            <div class="col-12">
+              <label for="Ename_type" class="form-label">ชื่อประเภทสินค้า</label>
+              <input type="text" class="form-control" id="Ename_type" name="Ename_type">
+            </div>
+            <div class="col-12">
+              <label for="Estatus_type" class="form-label">สถานะ</label>
+              <select class="form-select" id="Estatus_type" name="Estatus_type" aria-label="Estatus_type">
+                <?php foreach ($status as $key => $value) { ?>
+                  <option value="<?= $key ?>">
+                    <?= $value ?>
+                  </option>
+                <?php } ?>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
+          <a class="btn btn-warning" onclick="edit('edit_bank')">แก้ไข</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">จัดการข้อมูลผู้ใช้งาน</h5>
+
+            <table id='bank_data' class="table" style="font-family: 'Kanit', sans-serif;">
+              <thead>
+                <tr>
+                  <th>ลำดับ</th>
+                  <th>เลขที่บัญชี</th>
+                  <th>
+                    <b>ธนาคาร</b>
+                  </th>
+                  <th>สาขาเปิดบัญชี</th>
+                  <th>ชื่อบัญชี</th>
+                  <th>สถานะ</th>
+                  <th>จัดการ</th>
+                </tr>
+              </thead>
+              <tbody class='data-bank'>
+
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 
 
 
-  </main><!-- End #main -->
+</main><!-- End #main -->
